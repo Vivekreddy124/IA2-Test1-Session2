d7034579ff72a7c84eadf5c3be9dcabb82b454e2
Vivekreddy124 <95470129+Vivekreddy124@users.noreply.github.com>
2022-03-21 07:10:11 +0000

    p3final.c

--- p3final.c
+++ p3final.c
@@ -6,4 +6,26 @@ int input_number()
   scanf("%d",&num);
   return num;
 }
-int is_comp
+int is_composite(int n)
+{
+  for(int i=2;i<=n/2;i++)
+  {
+    if (n%i<=0)
+     return 1; 
+  }
+  return 0;
+  }
+void output(int n,int is composite)
+{
+  if(is composite)
+    printf("%d is a composite number\n",n);
+  else
+    printf("%D is not a composite number\n",n);
+}
+int main()
+{
+  int num=input_number();
+  int a=is_composite(num);
+  output(num,a);
+  return 0;
+}
